cadc9f6451c8b91a761721b5149fda3d8386f1c0
Ying Quan Tan <ying.quan.tan@gmail.com>
2012-04-21 12:20:37 -0400

    initial commit

---
+++ beatsalad/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  beatsalad
+//
+//  Created by Ying Quan Tan on 4/21/12.
+//  Copyright (c) 2012 Burning Hollow Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ beatsalad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  beatsalad
+//
+//  Created by Ying Quan Tan on 4/21/12.
+//  Copyright (c) 2012 Burning Hollow Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
