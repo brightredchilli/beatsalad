582fa4fedf44e26bf506bee446f30ba0b16644c5
Colin Cowles <colin@intrepid-dev.com>
2012-04-21 16:59:24 -0400

    made some touches to visualization animation

--- beatsalad/VisualizationViewController.h
+++ beatsalad/VisualizationViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "Track.h"
 #import "VisualizationView.h"
+#import "QuartzCore/CAAnimation.h"
 
 @interface VisualizationViewController : UIViewController {
     NSArray *trackArray;
--- beatsalad/VisualizationViewController.m
+++ beatsalad/VisualizationViewController.m
@@ -41,8 +41,8 @@
         Track *t = [[Track alloc] initWithColor:[UIColor redColor]];
         Track *t2 = [[Track alloc] initWithColor:[UIColor blueColor]];
         Track *t3 = [[Track alloc] initWithColor:[UIColor greenColor]];
-        Track *t4 = [[Track alloc] initWithColor:[UIColor blackColor]];
-        self.trackArray = [NSArray arrayWithObjects:t,t2,t3,t4,nil];
+//        Track *t4 = [[Track alloc] initWithColor:[UIColor blackColor]];
+        self.trackArray = [NSArray arrayWithObjects:t,t2,t3,nil];
     }
     
     int trackSize = 480 / [trackArray count];
@@ -50,14 +50,14 @@
     
     int i = 1;
     for(Track *t in trackArray) {
-        VisualizationView *v = [[VisualizationView alloc] initWithFrame:CGRectMake(0, -(trackSize * i), 320, trackSize)];
+        VisualizationView *v = [[VisualizationView alloc] initWithFrame:CGRectMake(0, -(trackSize * i) - 480, 320, trackSize + 480)];
         v.color = [t.trackColor copy];
         [self.view addSubview:v];
         [visArray addObject:v];
         ++i;
     }
     
-    float dur = 0.4;
+    float dur = 0.1;
     for(VisualizationView *v in visArray) {
         [UIView setAnimationDelegate:self];
         [UIView beginAnimations:nil context:NULL];
@@ -65,7 +65,24 @@
         [UIView setAnimationCurve:UIViewAnimationCurveLinear];
         v.transform = CGAffineTransformMakeTranslation(0, 480);
         [UIView commitAnimations];
-        dur = dur + 0.3;
+        dur = dur + 0.2;
+    }
+    
+    CABasicAnimation *bounceAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
+    bounceAnimation.duration = 0.2;
+    bounceAnimation.fromValue = [NSNumber numberWithInt:0];
+    bounceAnimation.toValue = [NSNumber numberWithInt:20];
+    bounceAnimation.repeatCount = 1;
+    bounceAnimation.autoreverses = YES;
+    bounceAnimation.fillMode = kCAFillModeForwards;
+    bounceAnimation.removedOnCompletion = NO;
+    bounceAnimation.additive = YES;
+    for(VisualizationView *v in visArray) {
+        [v.layer addAnimation:bounceAnimation forKey:@"bounceAnimation"];
+        [UIView beginAnimations:nil context:NULL];
+        [UIView setAnimationDuration:0.7];
+        [UIView setAnimationCurve:UIViewAnimationOptionCurveEaseOut];
+        [UIView commitAnimations];
     }
     // Do any additional setup after loading the view from its nib.
 }
