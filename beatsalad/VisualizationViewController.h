0d9e22744f960e62135dbca39f595c025210dc18
Colin Cowles <colin@intrepid-dev.com>
2012-04-21 13:49:28 -0400

    test commit

---
+++ beatsalad/VisualizationViewController.h
@@ -0,0 +1,13 @@
+//
+//  VisualizationViewController.h
+//  beatsalad
+//
+//  Created by Colin Cowles on 4/21/12.
+//  Copyright (c) 2012 Burning Hollow Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VisualizationViewController : UIViewController
+
+@end
