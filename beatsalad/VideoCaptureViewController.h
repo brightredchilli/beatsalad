f14f7227b67c7ca58b9641981b29565198cb814c
Ying Quan Tan <ying.quan.tan@gmail.com>
2012-04-21 13:10:28 -0400

    got some rudimentary video capture to work

---
+++ beatsalad/VideoCaptureViewController.h
@@ -0,0 +1,23 @@
+//
+//  VideoCaptureViewController.h
+//  beatsalad
+//
+//  Created by Ying Quan Tan on 4/21/12.
+//  Copyright (c) 2012 Burning Hollow Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+
+@interface VideoCaptureViewController : UIViewController<AVCaptureVideoDataOutputSampleBufferDelegate> {
+ 
+  AVCaptureSession *videoSession;
+  CALayer *videoLayer;
+  AVCaptureVideoPreviewLayer *prevLayer;
+  
+}
+
+
+
+@end
---
+++ beatsalad/VideoCaptureViewController.m
@@ -0,0 +1,148 @@
+//
+//  VideoCaptureViewController.m
+//  beatsalad
+//
+//  Created by Ying Quan Tan on 4/21/12.
+//  Copyright (c) 2012 Burning Hollow Technologies. All rights reserved.
+//
+
+#import "VideoCaptureViewController.h"
+
+@interface VideoCaptureViewController ()
+- (void)initCapture;
+@end
+
+@implementation VideoCaptureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  [self initCapture];
+}
+
+- (void)viewDidUnload
+{
+  
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark Implementation
+
+- (void)initCapture {
+  AVCaptureDeviceInput *input = [[AVCaptureDeviceInput alloc] initWithDevice:[AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo] error:nil];
+  
+  AVCaptureVideoDataOutput *outputCapture = [[AVCaptureVideoDataOutput alloc] init];
+  outputCapture.alwaysDiscardsLateVideoFrames  =YES;
+
+  
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    dispatch_queue_t queue;
+    queue = dispatch_queue_create("cameraQueue", NULL);
+    [outputCapture setSampleBufferDelegate:self queue:queue];
+    dispatch_release(queue);
+  });
+  
+  NSString* key = (NSString*)kCVPixelBufferPixelFormatTypeKey; 
+	NSNumber* value = [NSNumber numberWithUnsignedInt:kCVPixelFormatType_32BGRA]; 
+	NSDictionary* videoSettings = [NSDictionary dictionaryWithObject:value forKey:key]; 
+	[outputCapture setVideoSettings:videoSettings]; 
+	/*And we create a capture session*/
+	videoSession = [[AVCaptureSession alloc] init];
+	/*We add input and output*/
+	[videoSession addInput:input];
+	[videoSession addOutput:outputCapture];
+  /*We use medium quality, ont the iPhone 4 this demo would be laging too much, the conversion in UIImage and CGImage demands too much ressources for a 720p resolution.*/
+  [videoSession setSessionPreset:AVCaptureSessionPresetMedium];
+
+//	/*We add the Custom Layer (We need to change the orientation of the layer so that the video is displayed correctly)*/
+	videoLayer = [CALayer layer];
+	videoLayer.frame = self.view.bounds;
+	videoLayer.transform = CATransform3DRotate(CATransform3DIdentity, M_PI/2.0f, 0, 0, 1);
+	videoLayer.contentsGravity = kCAGravityResizeAspectFill;
+	[self.view.layer addSublayer:videoLayer];
+//	/*We add the imageView*/
+//	self.imageView = [[UIImageView alloc] init];
+//	self.imageView.frame = CGRectMake(0, 0, 100, 100);
+//  [self.view addSubview:self.imageView];
+//	/*We add the preview layer*/
+//	prevLayer = [AVCaptureVideoPreviewLayer layerWithSession:videoSession];
+//	prevLayer.frame = CGRectMake(100, 0, 100, 100);
+//  
+//	prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+//	[self.view.layer addSublayer:prevLayer];
+//	/*We start the capture*/
+	[videoSession startRunning];
+  
+
+}
+
+#pragma mark - AVCaptureVideoDataOutputSampleBufferDelegate
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput 
+didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer 
+       fromConnection:(AVCaptureConnection *)connection {
+    
+  
+
+	
+  CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer); 
+  /*Lock the image buffer*/
+  CVPixelBufferLockBaseAddress(imageBuffer,0); 
+  /*Get information about the image*/
+  uint8_t *baseAddress = (uint8_t *)CVPixelBufferGetBaseAddress(imageBuffer); 
+  size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer); 
+  size_t width = CVPixelBufferGetWidth(imageBuffer); 
+  size_t height = CVPixelBufferGetHeight(imageBuffer);  
+  
+  /*Create a CGImageRef from the CVImageBufferRef*/
+  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB(); 
+  CGContextRef newContext = CGBitmapContextCreate(baseAddress, width, height, 8, bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+  CGImageRef newImage = CGBitmapContextCreateImage(newContext); 
+	
+  /*We release some components*/
+  CGContextRelease(newContext); 
+  CGColorSpaceRelease(colorSpace);
+  
+  /*We display the result on the custom layer. All the display stuff must be done in the main thread because
+	 UIKit is no thread safe, and as we are not in the main thread (remember we didn't use the main_queue)
+	 we use performSelectorOnMainThread to call our CALayer and tell it to display the CGImage.*/
+	[videoLayer performSelectorOnMainThread:@selector(setContents:) withObject: (__bridge id)newImage waitUntilDone:YES];
+	
+	/*We display the result on the image view (We need to change the orientation of the image so that the video is displayed correctly).
+	 Same thing as for the CALayer we are not in the main thread so ...*/
+	//UIImage *image= [UIImage imageWithCGImage:newImage scale:1.0 orientation:UIImageOrientationRight];
+	
+	/*We relase the CGImageRef*/
+	CGImageRelease(newImage);
+	
+	//[self.imageView performSelectorOnMainThread:@selector(setImage:) withObject:image waitUntilDone:YES];
+	
+	/*We unlock the  image buffer*/
+	CVPixelBufferUnlockBaseAddress(imageBuffer,0);
+	
+}
+
+
+
+- (void)dealloc {
+ 
+  
+}
+
+@end
